1d3ce1ab850ca4d983b2d36c85e3cb85c46f5729
Uzaif M <uzaifm127@gmail.com>
2025-02-16 14:51:13 +0530

    resolved the params issue in client side on Poll page

--- src/app/poll/[pollId]/page.tsx
+++ src/app/poll/[pollId]/page.tsx
@@ -1,6 +1,6 @@
 import { PollClient } from "./page-client";
 
-export default function Poll({ params }: { params: { pollId: string } }) {
+export default async function Poll({ params }: { params: { pollId: string } }) {
   const { pollId } = params;
 
   return <PollClient pollId={pollId} />;
